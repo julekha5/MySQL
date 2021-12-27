3229967201d7cd76cb44b9f8801e4768bfaa3a34
Julekha5 <mulani.julekha93@gmail.com>
2021-12-27 19:14:36 +0530

    Refactor : created employee payroll table

--- payroll_service_database.sql
+++ payroll_service_database.sql
@@ -1,2 +1,8 @@
 create database payroll_service;
 use payroll_service;
+create table employee_payroll(id int not null auto_increment, 
+							  name varchar(150),
+							  salary double not null,
+                              startDate date not null,
+                              primary key (id) 
+                              );
